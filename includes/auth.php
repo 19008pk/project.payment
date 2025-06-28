c8b1970f787d82cfaaf1211248e89d23cc0adbd1
Praveen Yadav <praveen.19008@gmail.com>
2025-06-28 20:26:00 +0530

    error loggin added

--- includes/auth.php
+++ includes/auth.php
@@ -1,4 +1,15 @@
 <?php
+
+// Log all errors, notices, and warnings
+error_reporting(E_ALL);
+
+// Don't display errors to the browser (production-safe)
+ini_set('display_errors', 0);
+
+// Log all errors to a file inside the project
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/../error.log');
+
 session_start();
 
 function is_logged_in()
